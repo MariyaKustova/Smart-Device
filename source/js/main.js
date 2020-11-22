97eaff9642665c5d520d8fea106726bdf4d3c043
Маша <masha.kustova-guselnikova@yandex.ru>
2020-11-22 07:27:02 +0500

    Готовит компоненты

---
+++ source/js/main.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// var body = document.querySelector('body');
+// var buttonMenu = body.querySelector('.main-menu__button');
+// var menu = body.querySelector('.page-header');
+
+// body.classList.remove('no-js');
+
+// buttonMenu.addEventListener('click', function () {
+//   menu.classList.toggle('page-header--opened');
+// });
+
+// window.addEventListener('keydown', function (evt) {
+//   if (evt.keyCode === 27) {
+//     menu.classList.remove('page-header--opened');
+//   }
+// });
+
+// Picture element HTML5 shiv
+document.createElement('picture');
